97ba88929c1aa88e21fbde1d23b0a8f266eedd92
Andres <and.694@gmail.com>
2022-08-18 22:02:45 -0500

    static methods, class methods

--- talleres/patrones-poo/classes.py
+++ talleres/patrones-poo/classes.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from typing import Dict
 
 MATH = "math"
 BIOLOGY = "biology"
@@ -7,7 +8,7 @@ SPANISH = "spanish"
 
 class Person:
     """
-    Class Person 
+    Person Class
     """
 
     def __init__(self, name, last_name, birthday, id):
@@ -24,6 +25,7 @@ class Student(Person):
     """
 
     __MAX_GRADES = 3
+    __COUNT_ID = 1
 
     def __init__(self, name, last_name, birthday, id):
         super().__init__(name, last_name, birthday, id)
@@ -34,10 +36,17 @@ class Student(Person):
     def __str__(self) -> str:
         return f"""
         Estudiante: {self.name} {self.last_name}
+        id: {self.id}
+        date: {self.to_dash_date(self.birthday)}
         Biología: {self.biology}
-        Matemáticas: {self.math}
         Español: {self.spanish}
+        Matemáticas: {self.math}
         """
+
+    @classmethod
+    def from_dict(cls, obj):
+        cls.__COUNT_ID += 1
+        return cls(obj["name"], obj["last_name"], obj["birthday"], cls.__COUNT_ID)
 
     def add_grade(self, grade: float, course: str) -> None:
         course = getattr(self, course)
@@ -46,7 +55,12 @@ class Student(Person):
         else:
             raise Exception("Máximo número de notas agregadas")
 
-    def calc_avg(self, course: list) -> float:
+    @staticmethod
+    def to_dash_date(date: str) -> str:
+        return date.replace("/", "-")
+
+    @staticmethod
+    def calc_avg(course: list) -> float:
         return reduce(lambda a, b: a + b, course) / len(course)
 
     def get_mean(self, course: str) -> str:
